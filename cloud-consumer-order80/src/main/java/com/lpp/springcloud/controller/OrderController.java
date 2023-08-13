2050f7f070d3e97a09f5d75d9529313ca759fcbc
pingping.l <2314195288@qq.com>
2023-08-14 00:01:24 +0800

    修改消费者模块中的url以及请求方式

--- cloud-consumer-order80/src/main/java/com/lpp/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/lpp/springcloud/controller/OrderController.java
@@ -9,10 +9,7 @@ import com.lpp.springcloud.entities.CommonResult;
 import com.lpp.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -23,13 +20,13 @@ public class OrderController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @PostMapping("/order/create")
+    @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment)
     {
         return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
     }
 
-    @GetMapping("/order/find/{id}")
+    @GetMapping("/consumer/payment/find/{id}")
     public CommonResult<Payment> find(@PathVariable("id") int id)
     {
         return restTemplate.getForObject(PAYMENT_URL+"/payment/find/"+id,CommonResult.class);
